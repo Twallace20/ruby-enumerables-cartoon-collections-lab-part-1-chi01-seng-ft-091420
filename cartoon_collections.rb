c75b6f9cb1eeae45f10a5f5d5127e245da4fc785
Tyler Wallace <tywallace2017@gmail.com>
2020-09-08 14:27:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ def greet_characters(array)
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
-greet_characters = ["Dopey", "Oapey"]
+greet_characters = ["Hello Dopey!"]
 greet_characters.each do |element|
   puts element
   end 
